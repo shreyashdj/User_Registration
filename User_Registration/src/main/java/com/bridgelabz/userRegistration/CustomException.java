5b16a6157369877d19b65ce693aac5fdc42b8f2c
shreyashdj <shreyashjadhav1799@gmail.com>
2022-10-03 07:57:55 +0530

    [Shreyash]: Added code to create CustomException class which extends Exception class

---
+++ User_Registration/src/main/java/com/bridgelabz/userRegistration/CustomException.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.userRegistration;
+
+/**
+ * @author - Shreyash Jadhav
+ */
+public class CustomException extends Exception{
+    /**
+     *  created constructor with parameter String message
+     */
+    public CustomException(String message){
+        super(message);
+    }
+}
